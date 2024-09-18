4d6bbd72eea7788b737a3d350df7fc956d5eeb53
Balaji2624 <balajisapkal3062001@gmail.com>
2024-09-18 08:47:50 +0530

    Added: validate last name that start with cap character and minimum character.

--- src/main/java/com/bridgelabz/userregistration/UserRegistration.java
+++ src/main/java/com/bridgelabz/userregistration/UserRegistration.java
@@ -3,14 +3,21 @@
 package com.bridgelabz.userregistration;
 
 public class UserRegistration {
+    // validate first name
     public boolean isValidFirstName(String firstName) {
 
         String regex = "^[A-Z][a-zA-Z]{2,}$";
         return firstName.matches(regex);
     }
+     // validate last name
+     public boolean isValidLastName(String lastName) {
 
+         String regex = "^[A-Z][a-zA-Z]{2,}$";
+         return lastName.matches(regex);
+     }
     public static void main(String[] args) {
         UserRegistration userRegistration=new UserRegistration();
         System.out.println(userRegistration.isValidFirstName("balaji"));
+        System.out.println(userRegistration.isValidLastName("Sapkal"));
     }
 }
--- src/test/java/com/bridgelabz/userregistration/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/userregistration/UserRegistrationTest.java
@@ -6,6 +6,7 @@ import junit.framework.Assert;
 import org.junit.jupiter.api.Test;
 
 public class UserRegistrationTest {
+    // test first name
     @Test
     public void testValidFirstName() {
         UserRegistration user=new UserRegistration();
@@ -16,6 +17,21 @@ public class UserRegistrationTest {
     public void testInvalidFirstName() {
         UserRegistration user=new UserRegistration();
         boolean result=user.isValidFirstName("balaji");
+        Assert.assertFalse(result);
+    }
+
+    // test last name
+    @Test
+    public void testValidLastName() {
+        UserRegistration user=new UserRegistration();
+        boolean result=user.isValidLastName("Sapkal");
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void testInvalidLastName() {
+        UserRegistration user=new UserRegistration();
+        boolean result=user.isValidLastName("sapkal");
         Assert.assertFalse(result);
     }
 }
